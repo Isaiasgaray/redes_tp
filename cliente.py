59b276a6d45acce07ffda1ba87c3dfe2571d662b
Isaías <isaiasgaraygorta@gmail.com>
2023-07-01 17:54:29 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -1,19 +1,46 @@
 import requests
 
-url = "http://localhost:8000"  # url
+URL = 'http://localhost:8000'
+
+try:
+    requests.get(URL)
+except:
+    raise Exception('No se pudo abrir la URL.')
 
 def menu():
     print("Elija una opción en la API")
     print("1. Agregar libro")
     print("2. Eliminar libro")
-    print("3. Ver imagen del libro")
+    print("3. Descargar imagen de libro")
     print("4. Modificar libro")
     print("5. Ver libro por título")
     print("6. Ver libro por país")
-    print("7. Ver todos los libros")
-    print("8. Salir")
+    print("7. Ver libro por idioma")
+    print("8. Ver todos los libros")
+    print("9. Salir")
+
+def print_book(book):
+    headers = \
+        [
+            ('Autor', 'author'),
+            ('País', 'country'),
+            ('Idioma', 'language'),
+            ('Páginas', 'pages'),
+            ('Título', 'title'),
+            ('Año', 'year'),
+            ('ID', 'id'),
+        ]
+
+    print('-' * 20)
+    for atr, key in headers:
+        print(f'{atr}: {book[key]}')
+
+    print('-' * 20)
+    print()
+
 
 while True:
+    print()
     menu()
     opcion = input("Ingrese una opción: ")
 
@@ -21,87 +48,114 @@ while True:
         # Agregar libro
         book_data = {
             "author": input("Autor: "),
-            "country": input("Pais: "),
-            "imageLink": input("Enlace de la imagen: "),
+            "country": input("País: "),
+            # "imageLink": input("Enlace de la imagen: "),
             "language": input("Idioma: "),
             "link": input("Enlace: "),
-            "pages": int(input("Paginas: ")),
-            "title": input("Titulo: "),
-            "year": int(input("Año: "))
+            "pages": int(input("Páginas: ") or 0),
+            "title": input("Título: "),
+            "year": int(input("Año: ") or 0)
         }
-        response = requests.post(f"{url}/libros", json=book_data)
-        print(response.text)
+        response = requests.post(f"{URL}/libros", json=book_data)
+        print(f'\n{response.text}')
     
     elif opcion == "2":
         # Eliminar libro
         book_id = int(input("Ingrese el ID del libro a eliminar: "))
-        response = requests.delete(f"{url}/libros/{book_id}")
-        print(response.text)
+        response = requests.delete(f"{URL}/libros/{book_id}")
+        print(f'\n{response.text}')
     
     elif opcion == "3":
-        # Ver imagen del libro c:\Users\USER\Downloads
         book_id = int(input("Ingrese el ID del libro: "))
-        titulo = input("Ingresa el nonbre del archivo: ")
-        
-        response = requests.get(f"http://localhost:8000/img?book_id={book_id}", stream=True)
-        
-        with open(f"{titulo}.jpg", "wb") as file:
-            for lib in response.iter_content():
-                file.write(lib)
-        response.close()  
+        nombre = input("Ingrese el nombre para el archivo: ")
+
+        response = requests.get(
+                f"{URL}/libros/img/",
+                params={'book_id': book_id}
+                )
+
+        if response.status_code != 200:
+                print("\nNo se encontró la imagen del libro.")
+                continue
+
+        with open(f"{nombre}.png", "wb") as f:
+            f.write(response.content)
+
+        print("\nImagen descargada.")
 
     elif opcion == "4":
         # Modificar libro
         book_id = int(input("Ingrese el ID del libro a modificar: "))
         book_data = {
             "author": input("Autor (dejar en blanco si no desea modificar): "),
-            "country": input("Pais (dejar en blanco si no desea modificar): "),
+            "country": input("País (dejar en blanco si no desea modificar): "),
             "imageLink": input("Enlace de la imagen (dejar en blanco si no desea modificar): "),
             "language": input("Idioma (dejar en blanco si no desea modificar): "),
             "link": input("Enlace (dejar en blanco si no desea modificar): "),
-            "pages": input("Paginas (dejar en blanco si no desea modificar): "),
-            "title": input("Titulo (dejar en blanco si no desea modificar): "),
+            "pages": input("Páginas (dejar en blanco si no desea modificar): "),
+            "title": input("Título (dejar en blanco si no desea modificar): "),
             "year": input("Año (dejar en blanco si no desea modificar): ")
         }
         # Eliminar claves con valores vacíos
         book_data = {k: v for k, v in book_data.items() if v}
-        response = requests.put(f"{url}/libros/{book_id}", json=book_data)
-        print(response.text)
+        response = requests.put(f"{URL}/libros/{book_id}", json=book_data)
+        print(f'\n{response.text}')
     
     elif opcion == "5":
         # Ver libro por título
         book_title = input("Ingrese el título del libro a buscar: ")
-        response = requests.get(f"{url}/titulo/{book_title}")
-        if response.status_code == 200:
-            book = response.json()
-            print("Libro encontrado:")
-            print(book)
-        else:
-            print("Libro no encontrado.")
+        book_title = book_title.replace(' ', '%20')
+        response = requests.get(f"{URL}/libros/titulo/{book_title}")
+
+        if response.text == 'null':
+            print('\nLibro no encontrado.\n')
+            continue
+
+        book = response.json()
+        print("Libro encontrado:")
+        print_book(book)
     
     elif opcion == "6":
         # Ver libro por país
-        book_country = input("Ingrese el pais del libro a buscar: ")
-        response = requests.get(f"{url}/pais/{book_country}")
-        if response.status_code == 200:
-            books = response.json()
-            print(books)
-             
-        else:
-            print("No se encontraron libros para ese pais.")
-    
+        book_country = input("Ingrese el país del libro a buscar: ")
+        response = requests.get(f"{URL}/libros/pais/{book_country}").json()
+
+        if not response:
+            print("\nNo se encontraron libros para ese país.")
+            continue
+
+        print(f'Cantidad de libros: {len(response)}')
+
+        for book in response:
+            print_book(book)
+
     elif opcion == "7":
-        # Ver todos los libros
-        response = requests.get(f"{url}/")
-        books = response.json()
-        print("Todos los libros:")
-        for book in books:
-            print(book)
+        # Ver libro por idioma
+        lang = input("Ingrese el idioma del libro a buscar: ")
+        response = requests.get(f"{URL}/libros/lenguaje/{lang}").json()
+
+        if not response:
+            print("\nNo se encontraron libros para ese idioma.")
+            continue
+
+        print(f'Cantidad de libros: {len(response)}')
+
+        for book in response:
+            print_book(book)
     
     elif opcion == "8":
+        # Ver todos los libros
+        response = requests.get(f"{URL}/")
+        books = response.json()
+        print("\nTotal libros: {len(response)}\nTodos los libros: ")
+
+        for book in books:
+            print_book(book)
+    
+    elif opcion == "9":
         # Salir del programa
-        print("Saliendo...")
+        print("\nSaliendo...")
         break
 
     else:
-        print("Opcin invalida. Intente nuevamente.")
+        print("\nOpción inválida. Intente nuevamente.")
